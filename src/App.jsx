c46555771b8d7482eaaa7cd903852df65d270170
Arslan <arslanazhar00@gmail.com>
2023-05-10 00:34:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Contact from "./Contact";
+import Services from "./Services";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import About from "./About";
+import Footer from "./Footer";
+import Error from "./Error";
+
+import Dm from "./services/Dm";
+import Techsols from "./services/Techsols";
+import Sd from "./services/Sd";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />}>
+          <Route path="/my-services-website" element={<Home />} />
+        </Route>
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/services" element={<Services />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+        <Route path="/digitalmarketing" element={<Dm />} />
+        <Route path="/softwaredevelopment" element={<Sd />} />
+        <Route path="/digitalsolutions" element={<Techsols />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./footer.css";
+import { Error } from "@mui/icons-material";
+import fbLogo from "./graphics/ions/facebook.png";
+import linkedInLogo from "./graphics/ions/linkedin.png";
+import instgramLogo from "./graphics/ions/instagram.png";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  const Link = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bolder" : "normal",
+      textDecoration: "none",
+    };
+  };
+
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="data-1">
+              <p id="first-para">All Rights Reserved &copy; {year} </p>
+              <span>|</span>
+              <NavLink style={Link} to="*" element={<Error />}>
+                <p className="terms">Terms & Conditions</p>
+              </NavLink>
+            </div>
+
+            <div className="data-2">
+              <h3>Follow Us!</h3>
+              <div className="social-icons">
+                <NavLink
+                  to={"https://www.linkedin.com"}
+                  className="social-logo"
+                >
+                  <img
+                    src={linkedInLogo}
+                    style={{ width: "auto", height: "30px" }}
+                    alt="logo"
+                  />
+                </NavLink>
+                <NavLink to={"https://instagram.com/"} className="social-logo">
+                  <img
+                    src={instgramLogo}
+                    style={{ width: "auto", height: "30px" }}
+                    alt="logo"
+                  />
+                </NavLink>
+                <NavLink
+                  to={"https://www.facebook.com/"}
+                  className="social-logo"
+                  id="fbLogo"
+                >
+                  <img
+                    src={fbLogo}
+                    style={{ width: "auto", height: "30px" }}
+                    alt="logo"
+                  />
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
